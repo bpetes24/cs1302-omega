838c426325530755bbccf657cfc4dbbbeaba5fb1
Braden Peterson <bnp82380@uga.edu>
2021-12-12 18:29:43 -0500

    Added a GameBar class to display the score for a game of Pong.

---
+++ src/main/java/cs1302/game/GameBar.java
@@ -0,0 +1,21 @@
+package cs1302.game;
+
+import javafx.scene.control.ToolBar;
+import javafx.scene.text.Text;
+import javafx.scene.layout.HBox;
+
+/**
+ * This class is a {@link HBox} object with a {@link ToolBar} object
+ * containing the  score in a game of "Pong!" as well as additional
+ * instructions for how to play the game.
+ */
+public class GameBar extends HBox {
+
+    /**
+     * Constructs a {@code GameBar} object.
+     */
+    public GameBar() {
+        super();
+    } //GameBar
+
+} //class
