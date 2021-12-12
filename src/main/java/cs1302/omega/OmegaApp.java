4eff17fb0f49e02e62cf1f717f945b4a6c0bf356
Braden Peterson <bnp82380@uga.edu>
2021-12-12 18:08:13 -0500

    Modified OmegaApp class to create an instance of PongGame.

--- src/main/java/cs1302/omega/OmegaApp.java
+++ src/main/java/cs1302/omega/OmegaApp.java
@@ -1,18 +1,16 @@
 package cs1302.omega;
 
-import cs1302.game.DemoGame;
+import cs1302.game.PongGame;
 
 import javafx.application.Application;
 import javafx.application.Platform;
-import javafx.scene.control.Label;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
 import javafx.scene.layout.VBox;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 /**
- * REPLACE WITH NON-SHOUTING DESCRIPTION OF YOUR APP.
+ * This class is a JavaFX {@link Application} for running a {@link PongGame} object
+ * representing a game of "Pong!".
  */
 public class OmegaApp extends Application {
 
@@ -20,32 +18,20 @@ public class OmegaApp extends Application {
      * Constructs an {@code OmegaApp} object. This default (i.e., no argument)
      * constructor is executed in Step 2 of the JavaFX Application Life-Cycle.
      */
-    public OmegaApp() {}
+    public OmegaApp() {} //OmegaApp
 
     /** {@inheritDoc} */
     @Override
     public void start(Stage stage) {
 
-        // demonstrate how to load local asset using "file:resources/"
-        Image bannerImage = new Image("file:resources/readme-banner.png");
-        ImageView banner = new ImageView(bannerImage);
-        banner.setPreserveRatio(true);
-        banner.setFitWidth(640);
-
-        // some labels to display information
-        Label notice = new Label("Modify the starter code to suit your needs.");
-        Label instructions
-            = new Label("Move left/right with arrow keys; click rectangle to teleport.");
-
-        // demo game provided with the starter code
-        DemoGame game = new DemoGame(640, 240);
+        PongGame game = new PongGame(1280, 720);
 
         // setup scene
-        VBox root = new VBox(banner, notice, instructions, game);
+        VBox root = new VBox(game);
         Scene scene = new Scene(root);
 
         // setup stage
-        stage.setTitle("OmegaApp!");
+        stage.setTitle("OmegaApp: Pong!");
         stage.setScene(scene);
         stage.setOnCloseRequest(event -> Platform.exit());
         stage.sizeToScene();
@@ -56,4 +42,4 @@ public class OmegaApp extends Application {
 
     } // start
 
-} // OmegaApp
+} //class
