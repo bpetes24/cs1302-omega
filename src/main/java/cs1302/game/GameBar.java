d5c2bb6a93f86a6cebb09f6a9484ba3084ea3474
Braden Peterson <bnp82380@uga.edu>
2021-12-15 01:27:56 -0500

    Modified GameBar style. Needs work on displaying score boxes.

--- src/main/java/cs1302/game/GameBar.java
+++ src/main/java/cs1302/game/GameBar.java
@@ -1,9 +1,17 @@
 package cs1302.game;
 
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.paint.Color;
+import javafx.scene.layout.CornerRadii;
+import javafx.geometry.Insets;
+
 import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Text;
 import javafx.scene.text.TextAlignment;
+import javafx.scene.text.Font;
+import javafx.geometry.Pos;
 
 /**
  * This class is a {@link HBox} object containing the score
@@ -12,7 +20,12 @@ import javafx.scene.text.TextAlignment;
  */
 public class GameBar extends HBox {
 
-    private PongGame game;
+    private static final BackgroundFill FILL =
+        new BackgroundFill(Color.BLACK, CornerRadii.EMPTY, Insets.EMPTY);
+    private static final Background BG =
+        new Background(FILL);
+
+    private PongGame game;     //The pong game for this object
     private HBox scoreBoxOne;  //Score box for player one
     private HBox scoreBoxTwo;  //Score box for player two
     protected Text scoreOne;   //Score for player one
@@ -22,20 +35,24 @@ public class GameBar extends HBox {
      * Constructs a {@code GameBar} object.
      */
     public GameBar(PongGame game) {
-        super();
+        super(4);
         //Initialized default score and box for player one
         this.scoreBoxOne = new HBox();
         this.scoreOne = new Text("" + game.getPlayerOneScore());
+        scoreOne.setFont(new Font(20));
         scoreOne.setTextAlignment(TextAlignment.CENTER);
         scoreBoxOne.getChildren().add(scoreOne);
 
         //Initialized default score and box for player two
         this.scoreBoxTwo = new HBox();
         this.scoreTwo = new Text("" + game.getPlayerTwoScore());
+        scoreTwo.setFont(new Font(20));
         scoreTwo.setTextAlignment(TextAlignment.CENTER);
         scoreBoxTwo.getChildren().add(scoreTwo);
 
         //Sets children alignment and adds them to scene graph
+        this.setBackground(BG);
+        this.setAlignment(Pos.CENTER);
         this.getChildren().addAll(scoreBoxOne, scoreBoxTwo);
     } //GameBar
 
